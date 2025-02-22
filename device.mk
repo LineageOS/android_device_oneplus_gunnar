7ccf310d113f6c4f2e5d32315a8d45023efb0be3
Albert Tang <tangalbert919@yahoo.com>
2025-02-21 20:45:59 -0800

    gunnar: Build audio_amplifier
    Change-Id: Id7abcf2470f323a57ca5e9563016244d44c89211

--- device.mk
+++ device.mk
@@ -9,6 +9,9 @@ PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Audio
+PRODUCT_PACKAGES += \
+    audio_amplifier.qcom
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_ODM)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_holi/audio_platform_info_intcodec.xml \
