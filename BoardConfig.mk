957315261fd17ed371bb697fa9ca3257d5b00987
Bruno Martins <bgcngm@gmail.com>
2025-04-30 00:00:08 +0100

    gunnar: Set UDFPS extension lib via soong config
    Change-Id: I8a709bde4843b61420ea1003a749952d2619e2da

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,9 +12,6 @@ DEVICE_PATH := device/oneplus/gunnar
 # Audio
 AUDIO_FEATURE_ENABLED_EXT_AMPLIFIER := true
 
-# Fingerprint
-TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/oplus:libudfps_extension.oplus
-
 # Kernel
 BOARD_ONEPLUS_DYNAMIC_PARTITIONS_SIZE := 5591007232
 BOARD_SUPER_PARTITION_SIZE = 11190403072
